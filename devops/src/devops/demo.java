3af6c5ee1ead3f92bd1c171b8e2ff62835f61076
mohitha0407 <avula.mohitha@gmail.com>
2023-09-27 14:32:32 +0530

    demo

--- devops/src/devops/demo.java
+++ devops/src/devops/demo.java
@@ -4,7 +4,7 @@ public class demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("tis is first repository");
+		System.out.println("this is first repository");
 
 	}
 
